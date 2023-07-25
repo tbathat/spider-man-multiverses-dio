a8aa1d34f46a406d39614735eca595a19fb3b999
tbathat <tabathapradier@gmail.com>
2023-07-25 20:50:15 -0300

    feat: add event and transition to cards

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,31 @@
+function handleMouseEnter() {
+  this.classList.add(
+    "s-card--hoovered"
+  ); /* Modificação que vai ocorrer na função evento */
+}
+
+function handleMouseLeave() {
+  this.classList.remove("s-card--hoovered");
+}
+
+function addEventListenersToCard() {
+  const cardElements = document.getElementsByClassName("s-card");
+
+  for (let index = 0; index < cardElements.length; index++) {
+    const card = cardElements[index];
+    card.addEventListener(
+      "mouseenter",
+      handleMouseEnter
+    ); /* Evento que dar start na transformação da classe quando passar o mouse */
+    card.addEventListener(
+      'mouseleave',
+      handleMouseLeave
+    ); /* Evento que dar fim na transformação da classe quando passar o mouse */
+  }
+}
+/* Executar somente quando o html for renderizado totalmente */
+document.addEventListener(
+  "DOMContentLoaded",
+  addEventListenersToCard,
+  false
+); /* Ele vai aguardar esse evento dentro do método acontecer e vai chamar a função a seguir */
